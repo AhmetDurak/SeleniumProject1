5aacd25bf45e66b1ab42891fb5b0ca75ade2b1eb
Adrk51 <durakahmet049@gmail.com>
2022-06-26 23:54:22 +0200

    Webdriver

--- src/test/java/Utilities/Driver.java
+++ src/test/java/Utilities/Driver.java
@@ -11,6 +11,8 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
+import java.util.concurrent.TimeUnit;
+
 
 public class Driver {
 
@@ -25,6 +27,8 @@ public class Driver {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
+                    //driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
                 case "chrome-headless":
                     WebDriverManager.chromedriver().setup();
@@ -33,16 +37,19 @@ public class Driver {
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
                 case "firefox-headless":
                     WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver(new FirefoxOptions().setHeadless(true));
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
                 case "ie":
                     if (!System.getProperty("os.name").toLowerCase().contains("windows"))
                         throw new WebDriverException("Your OS doesn't support Internet Explorer");
                     WebDriverManager.iedriver().setup();
                     driver = new InternetExplorerDriver();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
 
                 case "edge":
@@ -50,6 +57,7 @@ public class Driver {
                         throw new WebDriverException("Your OS doesn't support Edge");
                     WebDriverManager.edgedriver().setup();
                     driver = new EdgeDriver();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
 
                 case "safari":
@@ -79,10 +87,28 @@ class myDriver{
     */
     private myDriver(){}
 
-
+    /*
+    * We make Webdriver private, because we want to close access from outside the class.
+    * We make it static because we use it in a static method*/
     private static WebDriver driver;
 
+    // Create a re-usable utility method which will return same driver instance when we call it
     public static WebDriver get(){
+        if (driver == null){
+            String browser = ConfigurationReader.get("url");
+            // Depending on the browserType that will be return from configuration.properties file
+            // switch statement will determine the case, and open the matching browser
+            switch (browser){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+
+                    break;
+            }
+
+        }
         return driver;
     }
 
---
+++ src/test/java/com/LoginSteps/Hook.java
@@ -0,0 +1,4 @@
+package com.LoginSteps;
+
+public class Hook {
+}
