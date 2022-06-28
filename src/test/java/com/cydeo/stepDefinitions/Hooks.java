31a761880082404a891a4dac5306b161df51f6ee
Adrk51 <durakahmet049@gmail.com>
2022-06-28 23:09:20 +0200

    Importing @Before and @After

--- src/test/java/com/cydeo/stepDefinitions/Hooks.java
+++ src/test/java/com/cydeo/stepDefinitions/Hooks.java
@@ -1,7 +1,8 @@
 package com.cydeo.stepDefinitions;
 
-import org.junit.After;
-import org.junit.Before;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import com.cydeo.Utilities.*;
@@ -12,9 +13,11 @@ import com.cydeo.Utilities.*;
 public class Hooks {
     Actions actions;
     WebDriverWait wait;
+
+    // It is important to be sure that @Before is imported from io.cucumber.java.Before
     @Before
     public void setUp(){
-        Driver.get();
+        Driver.get().get(ConfigurationReader.get("url"));
         actions = new Actions(Driver.get());
         wait = new WebDriverWait(Driver.get(), 10);
     }
