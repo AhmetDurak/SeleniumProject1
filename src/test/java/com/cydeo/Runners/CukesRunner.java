93d624551d70390b40c5d18ce0393d6100ec3a00
Adrk51 <durakahmet049@gmail.com>
2022-06-28 22:28:13 +0200

    Cucumber report plugin

--- src/test/java/com/cydeo/Runners/CukesRunner.java
+++ src/test/java/com/cydeo/Runners/CukesRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {},
+        plugin = "html:target/cucumber-report.html",
         features = "src/test/resources/features",
         glue = "com/cydeo/stepDefinitions",
         dryRun = false,
