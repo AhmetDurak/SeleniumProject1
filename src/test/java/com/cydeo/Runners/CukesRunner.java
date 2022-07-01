cfadfde540d3e85f933804cdff7d12d210a79be4
Adrk51 <durakahmet049@gmail.com>
2022-07-01 15:36:04 +0200

    paralel testing, maven-surefire-plugin

--- src/test/java/com/cydeo/Runners/CukesRunner.java
+++ src/test/java/com/cydeo/Runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cydeo/stepDefinitions",
         dryRun = false,
-        tags = "@LoginScenario"
+        tags = ""
 
 )
 
