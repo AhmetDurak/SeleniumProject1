5056eaa9e6ee1934965c2cd466b276bd99be405e
Adrk51 <durakahmet049@gmail.com>
2022-07-01 16:17:41 +0200

    change on project name

--- src/test/java/com/cydeo/Runners/CukesRunner.java
+++ src/test/java/com/cydeo/Runners/CukesRunner.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
-                "html:target/cucumber-report.html",
+                //"html:target/cucumber-report.html",
+                //"pretty",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber",
                 "json:target/cucumber.json"
@@ -15,7 +16,8 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cydeo/stepDefinitions",
         dryRun = false,
-        tags = ""
+        tags = "@DataTableTask",
+        publish = true
 
 )
 
